fd2e450d6d5a0939f0aefe0c6829a247161907f5
xmsxms5wn23 <114213357+GwakDoopal@users.noreply.github.com>
2022-12-23 06:39:22 +0900

    일당ㄴ은..

--- sketches/IMD222_final_template-master/works/work1/sketch.js
+++ sketches/IMD222_final_template-master/works/work1/sketch.js
@@ -1,42 +1,58 @@
-let tiles = [];
-
 function setup() {
   let boundingRects = document
     .getElementById("p5Canvas")
     .getBoundingClientRect();
   let canvas = createCanvas(boundingRects.width, boundingRects.height);
-  console.log(1);
-  console.log(
-    document.getElementById("p5Canvas").getBoundingClientRect().width
-  );
-  console.log(
-    document.getElementById("p5Canvas").getBoundingClientRect().height
-  );
   canvas.parent("p5Canvas");
-  let tileWidth = width / (howManyX + 1);
-  let tileHeight = height / (howManyY + 1);
-  for (let tileCntX = 0; tileCntX < howManyX; tileCntX++) {
-    for (let tileCntY = 0; tileCntY < howManyY; tileCntY++) {
-      let tileX = tileWidth * (tileCntX + 1) - tileWidth * 0.5;
-      let tileY = tileHeight * (tileCntY + 1) - tileHeight * 0.5;
-      let tile = new Tile(tileX, tileY, tileWidth, tileHeight);
-      tiles.push(tile);
-    }
-  }
 }
 
-let howManyX = 20;
-let howManyY = 20;
+let howManyX = 5;
+let howManyY = 5;
+let seedNum = 2;
+let noiseMult = 0.1;
 
 function mousePressed() {
-  tiles.forEach((tile) => {
-    tile.setStateByMouse(mouseX, mouseY);
-  });
+  seedNum = random(8);
 }
 
 function draw() {
-  background(255);
-  tiles.forEach((tile) => {
-    tile.render();
-  });
+  noiseMult = map(mouseX, 0, width, -0.1, 0.04);
+  noiseSeed(seedNum);
+  noStroke();
+  fill(255);
+  rect(0, 0, width, height);
+  let tileWidth = width / (howManyX + 1);
+  let tileHeight = height / (howManyY + 1);
+
+  for (let tileCntX = 0; tileCntX < howManyX; tileCntX++) {
+    for (let tileCntY = 0; tileCntY < howManyY; tileCntY++) {
+      let tileCenterX = tileWidth * (tileCntX + 1);
+      let tileCenterY = tileHeight * (tileCntY + 1);
+
+      let noiseVal = noise(tileCntX * noiseMult, tileCntY * noiseMult);
+      let noiseAngle = radians(360) * noiseVal;
+      let noiseColor = 255 * noiseVal;
+
+      let colorR = (255 * tileCntX) / (howManyX - 1);
+      let colorB = (255 * tileCntY) / (howManyY - 1);
+
+      push();
+      translate(tileCenterX, tileCenterY);
+      rotate(noiseAngle);
+      noFill();
+      stroke(colorR, colorG, colorB);
+      strokeWeight(1);
+      // line(0 - tileWidth * 0.5 + 5, 0, 0 + tileWidth * 0.5 - 5, 0);
+      // circle(mouseX * noiseMult, mouseY * noiseMult, 10);
+      triangle(
+        mouseX,
+        mouseX * noiseMult,
+        mouseY,
+        mouseY * noiseMult,
+        mouseX,
+        mouseY
+      );
+      pop();
+    }
+  }
 }
